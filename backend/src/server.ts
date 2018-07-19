f40341f6640f117c1851c22115ea6d05d2a0ac3e
Samantha Ashley <sla1031@gmail.com>
2018-07-19 15:29:12 -0400

    add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 import * as express from 'express';
 import * as parser from 'body-parser';
+import * as cors from 'cors';
 
 import { navigationRouter } from './api/routes/navigation';
 import { linkRouter } from './api/routes/link';
@@ -9,6 +10,7 @@ import { linkRouter } from './api/routes/link';
 const app: express.Express = express();
 const port: number = parseInt(process.env.PORT, 10) || 3000;
 
+app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
 
